bfb1b982417f321059f2dfa5257e70521af41972
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-16 22:06:28 +0300

    Updated App.java file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Rock Paper or Scissors");
+
+
+
+        try {
+            String input=bufferedReader.readLine();
+            RockPaperScissors game=new RockPaperScissors();
+            game.humanChooses(input);
+            game.computerChooses();
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
