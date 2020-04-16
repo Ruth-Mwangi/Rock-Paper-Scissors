d893bb91c669aad5afeb4e45a9e6565ae7cfd359
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-16 21:58:30 +0300

    Created a rock paper scissors app

---
+++ src/main/java/RockPaperScissors.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class RockPaperScissors {
+
+    public String computerChooses() {
+        String[] choice={"rock","paper","scissors"};
+        Random myRandomGenerator=new Random();
+
+        int random=myRandomGenerator.nextInt(3);
+        return choice[random];
+    }
+
+    public String humanChooses(String hChoise) {
+
+        hChoise="bloop";
+        return hChoise;
+    }
+
+    public String chooseWinner(String rock, String scissors) {
+
+        String winner = null;
+
+        if(rock.equals("rock")&&scissors.equals("scissors")){
+            winner="Rock";
+        }
+        return winner;
+    }
+}
---
+++ src/test/java/RockPaperScissorsTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class RockPaperScissorsTest {
+
+    @Test
+    public void computerChooses_shouldBeAString_true() throws Exception {
+        RockPaperScissors testGame = new RockPaperScissors();
+        String computerChoice =  testGame.computerChooses();
+        assertEquals(true, computerChoice instanceof String);
+    }
+
+    @Test
+    public void humanChooses_shouldBeAString_true() throws Exception{
+        RockPaperScissors testGame= new RockPaperScissors();
+        String humanChoice=testGame.humanChooses("rock");
+        assertEquals(true,humanChoice instanceof String);
+    }
+
+    @Test
+    public void chooseWinner_forRockAndScissors_Rock()throws Exception{
+        RockPaperScissors testGame=new RockPaperScissors();
+        String expectedValue="Rock";
+        assertEquals(expectedValue,testGame.chooseWinner("rock","scissors"));
+    }
+
+}
